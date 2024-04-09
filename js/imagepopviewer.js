200bccfaad6ce06c15d45323955a170343cf1b78
瀬長一高 <50620161+YigaoJp@users.noreply.github.com>
2024-04-09 13:22:06 +0900

    修正0409

--- js/imagepopviewer.js
+++ js/imagepopviewer.js
@@ -1,9 +1,18 @@
 $(function(){
+  var img_path = "./image/";
   const img_w = $('.ipv').width();
   const img_h = $('.ipv').height();
   var click_flg = false;
 
   $('body').prepend('<div id="ipv_bg"></div>');
+
+  var elem = $('.ipv');
+  var ipvArray = new Array();
+  var index = 0;
+  elem.each(function () {  
+    ipvArray[index] = $(this).children('img').attr('src');
+    index++;
+  });
 
   $(document).on("click", "#ipv_close_btn", function () {
     $(this).toggleClass("Click");
@@ -15,7 +24,12 @@ $(function(){
 
     $("#ipv_bg").empty();
     $("#ipv_bg").removeClass("Hide");
-    $("#ipv_bg").append('<div id="ipv_close_btn">✕</div>');
+    if(index > 0)
+    {
+      $("#ipv_bg").append('<div id="ipv_left_btn"></div>');
+      $("#ipv_bg").append('<div id="ipv_right_btn"></div>');
+    }
+    $("#ipv_bg").append('<div id="ipv_close_btn"></div>');
     $("#ipv_bg").append('<img src=\"' + src + '\">');
 
     $("#ipv_bg").toggleClass("Active");
@@ -24,4 +38,30 @@ $(function(){
   $("#ipv_bg").on("click", function() {
     $(this).toggleClass("Active");
   })
+});
+
+
+var clWidth = 0;
+var scaleWidth = 0;
+$(function(){
+  clWidth = document.documentElement.clientWidth;
+  scaleWidth = window.innerWidth;
+});
+
+document.documentElement.addEventListener('gesturestart', function (e) {
+
+  scaleWidth = window.innerWidth;
+
+  if(clWidth != scaleWidth) {
+		if($("#ipv_bg").find("#ipv_left_btn").hasClass('Hide') == false)
+    {
+      $("#ipv_bg").find("#ipv_left_btn").addClass('Hide');
+      $("#ipv_bg").find("#ipv_right_btn").addClass('Hide');
+    }
+    else 
+    {
+      $("#ipv_bg").find("#ipv_left_btn").removeClass('Hide');
+      $("#ipv_bg").find("#ipv_right_btn").removeClass('Hide');
+    }
+  }
 });
