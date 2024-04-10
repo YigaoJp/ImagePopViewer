3a68d24f339d1a7990e777c620e34d23660c9a22
瀬長一高 <50620161+YigaoJp@users.noreply.github.com>
2024-04-10 10:36:02 +0900

    調整中

--- js/imagepopviewer.js
+++ js/imagepopviewer.js
@@ -20,8 +20,6 @@ $(function(){
 
 	$(".ipv").on("click", function() {
 
-    var window_w = window.innerWidth;
-    var window_h = window.innerHeight;
     var src = $(this).children('img').attr('src');
 
     $("#ipv_bg").empty();
@@ -32,25 +30,40 @@ $(function(){
       $("#ipv_bg").append('<div id="ipv_right_btn"></div>');
     }
 
-    $("#ipv_bg").append('<img src=\"' + src + '\">');
+    $("#ipv_bg").append('<img src=\"' + ipvArray[0] + '\" alt="images" id="ipv_prev">');
+//    $("#ipv_bg").append('<img src=\"' + ipvArray[1] + '\" alt="images" id="main">');
+//    $("#ipv_bg").append('<img src=\"' + ipvArray[2] + '\" alt="images" id="ipv_next">');
+
     $("#ipv_bg").toggleClass("Active");
 
     $("#ipv_bg").append('<div id="ipv_close_btn"></div>');
-    var close_btn_w = parseInt($('#ipv_close_btn').css('width'));
-    var left_pos = ( (window_w / 2) + (parseInt($('#ipv_bg').find('img').css('width')) / 2) ) - close_btn_w - 8;
-    var top_pos = ( (window_h / 2) - (parseInt($('#ipv_bg').find('img').css('height')) / 2) ) + 4;
-    var close_btn = new Object();
-    close_btn.top = top_pos
-    close_btn.left = left_pos;
-    $("#ipv_close_btn").css(close_btn);
 
 	})
 
-  $("#ipv_bg").on("click", function() {
+  $(document).on("click", "#ipv_bg", function () {
     $(this).toggleClass("Active");
+  })
+
+  $(document).on("click", "#ipv_left_btn", function (e) {
+    // 親のイベントを無効
+    e.stopPropagation();
+    $("#ipv_prev").animate({left:"0", light: "0"},1000);
+    $('#ipv_prev').attr('id', 'main');
+    set_close_btn();
   })
 });
 
+function set_close_btn () {
+  var window_w = window.innerWidth;
+  var window_h = window.innerHeight;
+  var close_btn_w = parseInt($('#ipv_close_btn').css('width'));
+  var left_pos = ( (window_w / 2) + (parseInt($('#ipv_bg').find('img#main').css('width')) / 2) ) - close_btn_w - 8;
+  var top_pos = ( (window_h / 2) - (parseInt($('#ipv_bg').find('img#main').css('height')) / 2) ) + 4;
+  var close_btn = new Object();
+  close_btn.top = top_pos
+  close_btn.left = left_pos;
+  $("#ipv_close_btn").css(close_btn);
+}
 
 var clWidth = 0;
 var scaleWidth = 0;
